13a6311a0ca9ddff7737f403331a8d38591603cd
Deepakkumar S <dpkkumarjun7@Outlook.com>
2021-12-04 19:40:27 +0530

    iii

--- app/src/main/java/com/example/myassignmentguess/MainActivity.kt
+++ app/src/main/java/com/example/myassignmentguess/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myassignmentguess
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -10,43 +9,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val correct=Intent(this,EndGameActivity::class.java)
-        val wrong=Intent(this,WrongActivity::class.java)
-        val  crct=Intent(this,MainActivity::class.java)
 
 
-        var count=0
         val num:Int=700
 
         val guess=findViewById<TextInputLayout>(R.id.t1)?.editText?.text
         findViewById<TextView>(R.id.a1).setOnClickListener()
         {
-            count += 1
-
-            if(guess.toString().toInt()==num && count<=12)
+            if(guess.toString().toInt()==num)
             {
-               // findViewById<TextView>(R.id.a4).setText("You are right")
-startActivity(correct)
-
-
+                findViewById<TextView>(R.id.a4).setText("You are right")
             }
-             else if(guess.toString().toInt()!=num && count<=12)
+            else if(guess.toString().toInt()>num)
             {
-
-                findViewById<TextView>(R.id.a4).setText("You haven't yet find the answer")
-
+                findViewById<TextView>(R.id.a4).setText("No :) My number is Smaller")
             }
-
-
             else
             {
-                startActivity(wrong)
+
+                findViewById<TextView>(R.id.a4).setText("No :) My number is Bigger")
+
             }
-
-
-
-
-
 
 
 
