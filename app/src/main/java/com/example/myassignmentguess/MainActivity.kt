21ca2cd55e78136eca6b7e2573977dd02e059171
Deepakkumar S <dpkkumarjun7@Outlook.com>
2021-12-04 18:59:18 +0530

    iii

--- app/src/main/java/com/example/myassignmentguess/MainActivity.kt
+++ app/src/main/java/com/example/myassignmentguess/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myassignmentguess
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -9,27 +10,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val correct=Intent(this,EndGameActivity::class.java)
+        val wrong=Intent(this,WrongActivity::class.java)
+        val  crct=Intent(this,MainActivity::class.java)
 
 
+        var count=0
         val num:Int=700
 
         val guess=findViewById<TextInputLayout>(R.id.t1)?.editText?.text
         findViewById<TextView>(R.id.a1).setOnClickListener()
         {
-            if(guess.toString().toInt()==num)
+            count += 1
+
+            if(guess.toString().toInt()==num && count<=12)
             {
-                findViewById<TextView>(R.id.a4).setText("You are right")
+               // findViewById<TextView>(R.id.a4).setText("You are right")
+startActivity(correct)
+
+
             }
-            else if(guess.toString().toInt()>num)
+             else if(guess.toString().toInt()!=num && count<=12)
             {
-                findViewById<TextView>(R.id.a4).setText("No :) My number is Smaller")
+
+                findViewById<TextView>(R.id.a4).setText("You haven't yet find the answer")
+
             }
+
+
             else
             {
-
-                findViewById<TextView>(R.id.a4).setText("No :) My number is Bigger")
-
+                startActivity(wrong)
             }
+
+
+
+
+
 
 
 
---
+++ app/src/main/java/com/example/myassignmentguess/WrongActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myassignmentguess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WrongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wrong)
+    }
+}
